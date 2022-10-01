85fd49703a383d3db67e78fd9e3dfd9b7bc07392
jeffriveraaa <jeffrivera09@gmail.com>
2022-10-01 14:58:12 +0800

    3

--- myscripts.js
+++ myscripts.js
@@ -162,5 +162,12 @@ function animate() {
 ///////
 
 
-
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
 
